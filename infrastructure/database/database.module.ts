2c500fb5d8d018dfcc4e8b531f27d46bbe290fc6
journii <ginopaflo001608@gmail.com>
2026-04-30 10:29:02 -0500

    fix: import for datasource

--- infrastructure/database/database.module.ts
+++ infrastructure/database/database.module.ts
@@ -1,24 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppDataSource } from './data-source';
 
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: configService.get<any>('database.type'),
-        host: configService.get<string>('database.host'),
-        port: configService.get<number>('database.port'),
-        username: configService.get<string>('database.username'),
-        password: configService.get<string>('database.password'),
-        database: configService.get<string>('database.database'),
-        schema: configService.get<string>('database.schema'),
-        entities: [__dirname + '/../../src/modules/**/domain/entities/*.entity{.ts,.js}'],
-        synchronize: false,
-      }),
-      inject: [ConfigService],
-    }),
+    TypeOrmModule.forRoot(AppDataSource.options),
   ],
 })
 export class DatabaseModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import appConfig from '../infrastructure/config/app.config';
-import databaseConfig from '../infrastructure/config/database.config';
 import { DatabaseModule } from '../infrastructure/database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [appConfig, databaseConfig],
+      load: [appConfig],
     }),
     DatabaseModule,
   ],
