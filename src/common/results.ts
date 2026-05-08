d925c05e1429a68e3dfc1192fc78757b0a3df012
journii <ginopaflo001608@gmail.com>
2026-05-08 13:31:50 -0500

    doc: rename: entity

--- src/common/results.ts
+++ src/common/results.ts
@@ -1,5 +1,5 @@
-type Success<T> = { ok: true; value?: T };
-type Failure<E> = { ok: false; error: E };
+type success<T> = { ok: true; value?: T };
+type failure<E> = { ok: false; error: E };
 
-export type Result<T, E> = Success<T> | Failure<E>;
+export type Result<T, E> = success<T> | failure<E>;
 
